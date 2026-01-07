7a5574c7a16e776a3380f2c00b56c3d953e609ec
haluk01hub <halukdasdemir001@gmail.com>
2026-01-07 23:54:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,13 +49,19 @@ def index():
 def hazirla():
     video_url = request.form.get('url')
     # YouTube bot ve format engellerini aşmak için gelişmiş ayarlar
-    ydl_opts = {
-        'format': 'best', # En uygun formatı otomatik seçer
-        'outtmpl': 'indirilen_video.mp4',
+   ydl_opts = {
+        'format': 'best',
+        'outtmpl': 'video.mp4',
         'cookiefile': 'cookies.txt',
         'quiet': True,
         'no_warnings': True,
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        # Rastgele bir istemci gibi görünmek için başlıkları zorla
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-us,en;q=0.5',
+        }
+    }
     }
     
     try:
@@ -73,3 +79,4 @@ if __name__ == '__main__':
     # Render port ayarı
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
